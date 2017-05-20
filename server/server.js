e352bb175c0deaa372f43c35c5e90c6d26b914b1
Alvaro Rene <alvaro.kom@gmail.com>
2017-05-19 19:23:10 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const publicPath = path.join(__dirname, '../public')
+// console.log(__dirname + '../public')
+// console.log(publicPath)
+
+const port = process.env.PORT || 3000
+
+var app = express()
+
+app.use(express.static(publicPath))
+
+
+// app.get('/', (req, res) => {
+//     res.render('index')
+// })
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
